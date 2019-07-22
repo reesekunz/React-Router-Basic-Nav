11ded26fcdff43704c56ae825896e93d1b142c1d
Reese Kunz <reesekunz21@gmail.com>
2019-07-22 12:36:57 -0700

    setting up react router

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+// import your BrowserRouter as Router inside your `index.js` file.
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+// wrap your app with the router - renames component to router inside file 
+<Router>
+<App />
+</Router>,
+document.getElementById('root'));
